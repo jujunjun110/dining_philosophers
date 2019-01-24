31523cea8314c7fd5b6a97577699af2c117fb6bb
Jun Ito <jun_ito@voyagegroup.com>
2019-01-24 15:36:07 +0900

    Add: 哲学者問題の途中まで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// https://doc.rust-jp.rs/the-rust-programming-language-ja/1.6/book/dining-philosophers.html
+
+use std::thread;
+use std::time::Duration;
+
 struct Philosopher {
     name: String,
 }
@@ -8,12 +13,36 @@ impl Philosopher {
             name: name.to_string(),
         }
     }
+
+    fn eat(&self) {
+        println!("{} is eating", self.name);
+        thread::sleep(Duration::from_millis(1000));
+        println!("{} is done eating.", self.name);
+    }
 }
 
 fn main() {
-    let p1 = Philosopher::new("Judith Butler");
-    let p2 = Philosopher::new("Gilles Deleuze");
-    let p3 = Philosopher::new("Karl Marx");
-    let p4 = Philosopher::new("Emma Goldman");
-    let p5 = Philosopher::new("Michel Foucault");
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+
+    // for p in &philosophers {
+    //     p.eat()
+    // }
+    let handles: Vec<_> = philosophers
+        .into_iter()
+        .map(|p| {
+            thread::spawn(move || {
+                p.eat();
+            })
+        })
+        .collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
